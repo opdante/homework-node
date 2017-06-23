ae11fa3d597a2f4a36059c0db993e1f5f8fc11b4
opdante <opdante@gmail.com>
2017-06-23 12:22:00 -0700

    intia commit for eaze coding challenge

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict'
+const downloadPackages = require('lib/DownloadPackage');
+const scrapeNpm = require('');
 
 module.exports = downloadPackages
 
 function downloadPackages (count, callback) {
-
+    data = scrapeNpm(count);
+    downloadPackages.getPackages(data, './packages');
+    callback();
 }
