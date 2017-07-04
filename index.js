accd5abe1eb99ea92079cf4396bb5f7850e97bd8
opdante <opdante@gmail.com>
2017-07-04 14:59:03 -0700

    update gist url to get latest md file

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const scrapeNpm = require('./lib/ScrapeNpm.js');
 const readGist = require('./lib/ReadGist.js');
 const downloadGist = require('./lib/DownloadGistFile.js')
 
-var pageURL = 'https://www.npmjs.com/browse/depended',
-    textSource = 'https://gist.githubusercontent.com/anvaka/8e8fa57c7ee1350e3491/raw/77148a2cc39554483e789a4f3ccfc6d0120bb54e/01.most-dependent-upon.md',
-    path  = './lib/file.txt';
+var path  = './lib/file.txt';
 
 module.exports = downloadPackages
 
